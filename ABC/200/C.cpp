7983170988b168857d4d24c9e659ee8078d95e2a
m4oyu <yuki.yamamoto0915@gmail.com>
2021-05-09 02:31:53 +0900

    review

--- ABC/200/C.cpp
+++ ABC/200/C.cpp
@@ -6,27 +6,18 @@ using namespace std;
 
 
 int main() {
-      int N, count;
+      int N, A[200005];
+      long long B[200];
       cin >> N;
-      vector<int> A(200000);
       for (int i = 0; i < N; i++) {
             cin >> A[i];
+            B[A[i]%200]++;
       }
-
-      for (int i = 0; i < N; i++) {
-            int up = 0;
-            for (int n = i+1; n < N; n++)
-            {
-                  if (abs(A[i]-A[n]) % 200 == 0){
-                        A.erase(A.begin() + n + 1);
-                        up++;
-                        count += up;
-                        N--;
-                  }
-            }
+      long long res = 0;
+      for (int i = 0; i < 200; i++) {
+            res += (B[i]*(B[i]-1))/2;
       }
-      cout << count << endl;
-
+      cout << res << endl;
 
       return 0;
 }
