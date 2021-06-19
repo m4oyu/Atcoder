1967d6bbb57efda0113c1944e7eb4da0a02f5c5c
m4oyu <yuki.yamamoto0915@gmail.com>
2021-06-19 11:32:02 +0900

    sortして貪欲法

---
+++ Problem90/014.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    rep(i, n) cin >> a[i];
+    vector<int> b(n);
+    rep(i, n) cin >> b[i];
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    ll ans = 0;
+    rep(i, n) {
+        ans += abs(a[i]-b[i]);
+    }
+    cout << ans << endl;
+    return 0;
+}
