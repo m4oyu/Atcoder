0b11882ac461fe3ccb2d4c7e160c874965759bb3
m4oyu <yuki.yamamoto0915@gmail.com>
2023-03-16 03:12:40 +0900

    go 環境構築

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// reader := bufio.NewReader(os.Stdin)
+	// txt, _ := reader.ReadString(' ');
+	// txt2, _ :=
+
+	txt := "Hello world"
+	fmt.Println(txt)
+}
