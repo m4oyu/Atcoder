10fc02ead6dd755aed4c0f3345f8cc1c48d4855b
m4oyu <yuki.yamamoto0915@gmail.com>
2023-03-17 17:27:19 +0900

    update go debug env

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-func main() {
-	// reader := bufio.NewReader(os.Stdin)
-	// txt, _ := reader.ReadString(' ');
-	// txt2, _ :=
+var scanner = bufio.NewScanner(os.Stdin)
 
-	txt := "Hello world"
-	fmt.Println(txt)
+func init() {
+	if len(os.Args) >= 2 {
+		if os.Args[1] == "debug" {
+			debug()
+		}
+	}
+	const buf = 200100
+	scanner.Split(bufio.ScanWords)
+	scanner.Buffer(make([]byte, buf), buf)
+}
+
+func debug() {
+	inputFile, err := os.Open("./problem.in")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "There is no inputFile.")
+		os.Exit(1)
+	}
+
+	scanner = bufio.NewScanner(inputFile)
+}
+
+func main() {
 }
