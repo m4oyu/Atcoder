590148bdd24d62f146c5e97dad1be1c68bf935e4
m4oyu <yuki.yamamoto0915@gmail.com>
2023-03-18 16:12:23 +0900

    079 ac

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 var scanner = bufio.NewScanner(os.Stdin)
@@ -30,4 +31,61 @@ func debug() {
 }
 
 func main() {
+	var h, w int
+	scanner.Scan()
+	h, _ = strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	w, _ = strconv.Atoi(scanner.Text())
+	a := make([][]int, h)
+	for i, _ := range a {
+		tmp := make([]int, w)
+		for j := 0; j < w; j++ {
+			scanner.Scan()
+			tmp[j], _ = strconv.Atoi(scanner.Text())
+		}
+		a[i] = tmp
+	}
+	b := make([][]int, h)
+	for i, _ := range a {
+		tmp := make([]int, w)
+		for j := 0; j < w; j++ {
+			scanner.Scan()
+			tmp[j], _ = strconv.Atoi(scanner.Text())
+		}
+		b[i] = tmp
+	}
+
+	count := 0
+	for i := 0; i < h-1; i++ {
+		for j := 0; j < w-1; j++ {
+			diff := a[i][j] - b[i][j]
+			if diff != 0 {
+				a[i][j] -= diff
+				a[i+1][j] -= diff
+				a[i][j+1] -= diff
+				a[i+1][j+1] -= diff
+
+				if diff < 0 {
+					diff = -diff
+				}
+				count += diff
+			}
+		}
+	}
+
+	for i := 0; i < h; i++ {
+		if a[i][w-1] != b[i][w-1] {
+			fmt.Println("No")
+			return
+		}
+	}
+	for i := 0; i < w; i++ {
+		if a[h-1][i] != b[h-1][i] {
+			fmt.Println("No")
+			return
+		}
+	}
+
+	fmt.Println("Yes")
+	fmt.Println(count)
 }
