38cf384ba08283592f760745d05f793df7c8a88d
m4oyu <yuki.yamamoto0915@gmail.com>
2023-05-09 16:27:16 +0900

    file 整理

--- re_typical90/042.cpp
+++ re_typical90/042.cpp
@@ -1,34 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
 using ll = long long;
 
-int ANSMOD = pow(10, 9) + 7;
+ll mod = 1000000007;
 
-int main()
-{
+int main() {
     int k;
     cin >> k;
 
-    if (k % 9 != 0)
-    {
-        cout << "0" << endl;
+    if (k % 9 != 0) {
+        cout << 0 << endl;
         return 0;
     }
 
-    vector<ll> dp(k + 1);
+    vector<ll> dp(100020, 0);
     dp[0] = 1;
 
-    for (int i = 1; i <= k; i++)
-    {
-        int n = min(i, 9);
-
-        for (int j = 1; j <= n; j++)
-        {
-            dp[i] = (dp[i] + dp[i - j]) % ANSMOD;
+    for (int i = 0; i <= k; i++) {
+        for (int j = 1; j < 10; j++) {
+            dp[i + j] = (dp[i + j] + dp[i]) % mod;
         }
     }
 
     cout << dp[k] << endl;
+
     return 0;
 }
