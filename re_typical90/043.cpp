281b912de81f1516ef6c6e6337ed18c11bbb118a
m4oyu <yuki.yamamoto0915@gmail.com>
2023-05-10 17:24:09 +0900

    not ac

--- re_typical90/043.cpp
+++ re_typical90/043.cpp
@@ -1,30 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-    
-vector<vector<char>> m(1001, vector<char>(1001, 'n'));
-vector<vector<vector<int>>> c(1001, vector<vector<int>>(1001, vector<int>(4, -1)));
+
+
+int h, w, rs, cs, rt, ct;
+vector<vector<char>> m(1002, vector<char>(1002, 'n'));
+vector<vector<vector<int>>> c(1002, vector<vector<int>>(1002, vector<int>(4, 1000000)));
 pair<int, int> vec[4] = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+deque<vector<int>> deq;
 
-void solve(int row, int col, int dir) {
-    if (m[row][col] == 'n') {
+void solve(int row, int col, int dir, int count) {
+    if (c[row][col][dir] < count) {
         return;
-    } 
-
+    }
+   
     for (int i = 0; i < 4; i++)
     {
-        if (c[row][col][i] == -1) {
-            
+        c[row][col][i] = count + (dir == i ? 0 : 1);
+
+        int t_row = row+vec[i].first;
+        int t_col = col+vec[i].second;
+        if (m[t_row][t_col] != '.') {
+            continue;
+        }
+
+        if (dir == i) {
+            if (count < c[row][col][i]) {
+                deq.push_front({t_row, t_col, i, count});
+            }
+        } else {
+            if (count+1 < c[row][col][i]) {
+                deq.push_back({t_row, t_col, i, count+1});
+            }
         }
     }
-        
-    
-   
+
+    return;
 }
 
-
 int main() {
-    int h, w, rs, cs, rt, ct;
     cin >> h >> w >> rs >> cs >> rt >> ct;
     for (int i = 1; i <= h; i++)
     {
@@ -33,10 +47,48 @@ int main() {
             cin >> m[i][j];
         }
     }
+
+    for (int i = 0; i < 4; i++)
+    {
+        if (m[rs+vec[i].first][cs+vec[i].second] != '.') {
+            continue;
+        }
+        deq.push_back({rs, cs, i, 0});        
+    }
+
+    vector<int> a(4);   
+    while(!deq.empty()) {
+        a = deq.front();
+        deq.pop_front();
+        solve(a[0], a[1], a[2], a[3]);
+    }
     
+    int ans = 1000000;
+    for (int i = 0; i < 4; i++)
+    {
+        ans = min(ans, c[rt][ct][i]);
+    }
 
-
-
+    cout << ans << endl;
 
     return 0;
 }
+
+    // for (int i = 1; i <= h; i++)
+    // {
+    //     for (int j = 1; j <= w; j++)
+    //     {
+    //         if (m[i][j] == '#') {
+    //             cout << "#";
+    //         } else {
+    //             int out = 1000000;
+    //             for (int k = 0; k < 4; k++)
+    //             {
+    //                 out = min(out, c[i][j][k]);
+    //             }
+    //             cout << out;                
+    //         }
+    //     }
+    //     cout << endl;
+        
+    // }
